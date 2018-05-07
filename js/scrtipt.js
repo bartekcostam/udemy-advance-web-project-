70835ae29839a07529eaa9d0e4699b1da4ba6a1e
bartekcostam <bartekoffice1@gmail.com>
2018-05-08 00:00:53 +0100

    testing

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -1,70 +1,18 @@
-var age;
 
-age = 12;
-var userNumber = [];
 
-for(userNumber = 0; userNumber<10;userNumber++){
 
-    console.log(userNumber);
-    
-}
+var btn = document.querySelector('.btn');
 
-var i;
-var user = [];
-for (i=0;i<10;i++){
+function onBtnClick(event) {
 
-user[i] = userNumber[i] + 2;
+alert('przycisk klikniety');
 
-console.log(user);
-
+console.log('dziala');
+btn.removeEventListener('click', onBtnClick);
 
 }
 
-
-//Objekt - definiujemy obiekt 
-
-var czlowiek = {
-
-wzrost: 168,
-kolorOczu: 'niebieskie',
-kolorSkory: 'bialy',
-
-umyjDupsko: function(){
-
-    console.log('mysje duspko');
-
-},
-
-jedzSamochodem: function(){
-
-    console.log('jade sobie w pizdu');
-}
-
-}
-
-console.log(czlowiek);
-
-var nowyElement = document.createElement("div");
-
-var tresc = document.createTextNode("przycisk - nowy");
-
-nowyElement.appendChild(tresc);
-
-var element = document.getElementById("kolumny");
-
-  var click =  document.getElementById("test");
-
-  click.addEventListener("click",function () {
-
-element.appendChild(tresc);
-
-  }, false);
-
-var btn = document.querySelector('#btn');
-
-
-
-  
+  btn.addEventListener('click', onBtnClick);
   
 
 
