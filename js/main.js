7777c70bbbd1ddccfd9ab15b6544f28a782a1205
bartekcostam <bartekoffice1@gmail.com>
2018-05-09 22:13:32 +0100

    ajax - testing basic construction

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.getElementById('get-btn').addEventListener('click', getAjax);
+
+function getAjax() {
+
+const xhr = new XMLHttpRequest();
+
+
+xhr.onload = () => {
+
+if (xhr.status === 200){
+
+    document.getElementById('response').textContent = xhr.responseText;
+}
+
+}
+
+xhr.open('GET', 'text.txt', true);
+
+}
