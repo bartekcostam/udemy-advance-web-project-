e2306f4b86b56a449dea9a64f187a54f389094ea
bartekcostam <bartekoffice1@gmail.com>
2018-05-11 23:34:02 +0100

    array of obejcts JSON

--- main.js
+++ main.js
@@ -20,3 +20,23 @@ xhr.open('GET','text.txt',true);
 xhr.send();
 
 }
+
+document.getElementById('btn-json').addEventListener('click',getJson);
+
+function getJson() {
+
+const xhr = new XMLHttpRequest();
+
+xhr.onload = function(){
+
+
+    if(this.status === 200){
+
+const res = JSON.parse(this.responseText);
+
+console.log(res);
+
+       // document.querySelector('.response').textContent = res;
+    }
+}
+}
