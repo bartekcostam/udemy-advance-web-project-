ddc82370a8e5beed469f00f91a7e7bafa9253b11
bartekcostam <bartekoffice1@gmail.com>
2018-05-12 01:08:34 +0100

    JSON - Working

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -1,4 +1,25 @@
+document.querySelector('.btn-json').addEventListener('click',getJson);
 
+function getJson() {
+
+const abc = new XMLHttpRequest();
+
+abc.onload = function(){
+
+
+    if(this.status === 200){
+
+const res = JSON.parse(this.responseText);
+
+console.log(res);
+
+       // document.querySelector('.response').textContent = res;
+    }
+}
+
+abc.open('GET', 'users.json', true);
+abc.send();
+}
 
 
   
